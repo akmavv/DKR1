8940553d51909ea4d7f5a58e9fa6c1be631eabe1
akmavv <144677328+akmavv@users.noreply.github.com>
2023-10-12 12:17:40 +0300

    Add files via upload

---
+++ dkr1.pas
@@ -0,0 +1,17 @@
+﻿program funktia;
+
+var
+  x: real;
+
+begin
+  x := -12;
+  while x <= 8 do 
+  begin
+    if (x < -10) then writeln('x= ', ((x):1:1), ', y= ', ((cos(x) * x + cos(2 * x) / x):1:1)) else
+    if (x < -1) then writeln('x= ', ((x):1:1), ', y= ', ((ln(x) + 28):1:1)) else
+    if (x < 6) then writeln('x= ', ((x):1:1), ', y= ', 47 + 71) else
+      writeln('x= ', ((x):1:1), ', y= ', 39);
+    x := x + 0.2;
+  end;
+  
+end.
